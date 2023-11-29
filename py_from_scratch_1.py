91f794a0eeec95714085e49c5593ed8006193096
mgarsteck <g-photography@hotmail.com>
2023-11-28 23:47:05 -0500

    ch9 - Backpropagation

--- py_from_scratch_1.py
+++ py_from_scratch_1.py
@@ -43,11 +43,27 @@ class Layer_Dense:
     # Forward Pass
     def forward(self, inputs):
         self.output = np.dot(inputs, self.weights) + self.biases
+        self.inputs = inputs
+
+    def backward(self, dvalues):
+        # Gradients on parameters
+        self.dweights = np.dot(self.inputs.T, dvalues)
+        self.dbiases = np.sum(dvalues, axis=0, keepdims=True)
+        # Gradient on values
+        self.dinputs = np.dot(dvalues, self.weights.T)
 
 class Activation_ReLU:
     # Forward pass
     def forward(self, inputs):
+        self.inputs = inputs
         self.output = np.maximum(0, inputs)
+
+    def backward(self, dvalues):
+        self.dinputs = dvalues.copy()
+        # zero gradient where input values were negative
+        self.dinputs[self.inputs <= 0] = 0
+
+    
 
 # X, y = spiral_data(samples=100, classes=3)
 
@@ -132,6 +148,20 @@ class Activation_Softmax:
         probilities = exp_values / np.sum(exp_values, axis=1, keepdims=True)
 
         self.output = probilities
+    
+    # Backward pass
+    def backward(self, dvalues):
+
+        self.dinputs = np.empty_like(dvalues)
+
+        # enumerate outputs and inputs
+        for index, (single_output, single_dvalues) in enumerate(zip(self.output, dvalues)):
+            # Flatten output array
+            single_output = single_output.reshape(-1,1)
+            # Calculate Jacobian matric of the output
+            jacobian_matrix = np.diagflat(single_output) - np.dot(single_output, single_output.T)
+            # add it to the array of sample gradients
+            self.dinputs[index] = np.dot(jacobian_matrix, single_dvalues)
 
 
 # X, y = spiral_data(samples=100, classes=3)
@@ -180,7 +210,7 @@ class Activation_Softmax:
 # print(loss, loss_optimized)
 
 # -------------------------
-softmax_outputs = np.array([[0.7,0.1,0.2],
+softmax_outputs1 = np.array([[0.7,0.1,0.2],
                             [0.1,0.5,0.4],
                             [0.02,0.9,0.08]])
 
@@ -194,13 +224,13 @@ class_targets2 = np.array([[1,0,0],
                         [0,1,0]])
 
 # if len(class_targets2.shape) == 1:
-#     correct_confidences = softmax_outputs[
-#         range(len(softmax_outputs)),
+#     correct_confidences = softmax_outputs1[
+#         range(len(softmax_outputs1)),
 #         class_targets2
 #     ]
 # elif len(class_targets2.shape) == 2:
 #     correct_confidences = np.sum(
-#         softmax_outputs * class_targets2,
+#         softmax_outputs1 * class_targets2,
 #         axis=1
 #     )
 # neg_log = -np.log(correct_confidences)
@@ -245,6 +275,20 @@ class Loss_CategoricalCrossentropy(Loss):
         negative_log_likelihoods = -np.log(correct_confidences)
         return negative_log_likelihoods
     
+    def backward(self, dvalues, y_true):
+
+        samples = len(dvalues)
+
+        labels = len(dvalues[0])
+
+        # if labels are sparse, convert them to one-hot vector
+        if len(y_true.shape) == 1:
+            y_true = np.eye(labels)[y_true]
+        # calculate gradient
+        self.dinputs = -y_true / dvalues
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
+    
 # X, y = spiral_data(samples=100, classes=3)
 
 # # # Create dense layer with 2 input and 3 outpet
@@ -274,7 +318,7 @@ class Loss_CategoricalCrossentropy(Loss):
 
 # --------
 
-# predictions = np.argmax(softmax_outputs2, axis=1)
+# predictions = np.argmax(softmax_outputs12, axis=1)
 
 # if len(class_targets1.shape) == 2:
 #     class_targets1 = np.argmax(class_targets1, axis=1)
@@ -365,38 +409,185 @@ class Loss_CategoricalCrossentropy(Loss):
 # -----------------------------------------------
 
 
-def f(x):
-    return 2*x**2
+# def f(x):
+#     return 2*x**2
 
 
-# y = mx+b
-x = np.arange(0,5,0.001)
-y = f(x)
-p2_delta = 0.0001
-x1 = 2
-x2 = x1+p2_delta
-
-y1 = f(x1)
-y2 = f(x2)
-
-approximate_derivative = (y2-y1)/(x2-x1)
-b = y2 - approximate_derivative*x2
-
-def tangent_line(x):
-    return approximate_derivative*x + b
-
-to_plot = [x1-0.9, x1, x1+0.9]
-
-
-print('Approximate derivative for f(x)',
-      f'where x = {x1} is {approximate_derivative}')
-
-
-plt.plot(to_plot, [tangent_line(i) for i in to_plot])
-plt.plot(x,y)
-
-plt.show()
-# x = np.array(range(56))
+# # y = mx+b
+# x = np.arange(0,5,0.001)
 # y = f(x)
+# p2_delta = 0.0001
+# x1 = 2
+# x2 = x1+p2_delta
+
+# y1 = f(x1)
+# y2 = f(x2)
+
+# approximate_derivative = (y2-y1)/(x2-x1)
+# b = y2 - approximate_derivative*x2
+
+# def tangent_line(x):
+#     return approximate_derivative*x + b
+
+# to_plot = [x1-0.9, x1, x1+0.9]
 
 
+# print('Approximate derivative for f(x)',
+#       f'where x = {x1} is {approximate_derivative}')
+
+
+# plt.plot(to_plot, [tangent_line(i) for i in to_plot])
+# plt.plot(x,y)
+
+# plt.show()
+# # x = np.array(range(56))
+# # y = f(x)
+
+
+# -----------------------------------------------
+# 8. Gradients, partial derivatives, && chain rule
+# -----------------------------------------------
+
+# -----------------------------------------------
+# 9. Backpropagation
+# -----------------------------------------------
+
+# ReLU(sum(inputs * weights) + bias)
+
+# dvalues = np.array([[1.,1.,1.],
+#                     [2.,2.,2.],
+#                     [3.,3.,3.]])
+
+# inputs = np.array([[1,2,3,2.5],
+#                    [2.,5.,-1.,2],
+#                    [-1.5,2.7,3.3,-0.8]])
+
+# weights = np.array([[0.2,0.8,-0.5,1],
+#                     [0.5,-0.91,0.26,-0.5],
+#                     [-0.26,-0.27,0.17,0.87]]).T
+
+# biases = np.array([[2,3,0.5]])
+
+# layer_outputs = np.dot(inputs, weights) + biases
+# relu_outputs = np.maximum(0, layer_outputs)
+
+# drelu = relu_outputs.copy()
+# drelu[layer_outputs <= 0] = 0
+
+# dinputs = np.dot(drelu, weights.T)
+# dweights = np.dot(inputs.T, drelu)
+
+# dbiases = np.sum(drelu, axis=0, keepdims=True)
+
+# weights += -0.001 * dweights
+# biases += -0.001 * dbiases
+
+# print('Weights', weights, 'Biases', biases)
+
+# --------------
+
+# Softmax classifier - combined Softmax activation and cross-entropy loss for faster backward step
+class Activation_Softmax_Loss_CategoricalCrossentropy():
+
+    # Creates activations and loss function objects
+    def __init__(self):
+        self.activation = Activation_Softmax()
+        self.loss = Loss_CategoricalCrossentropy()
+    
+    # Forward pass
+    def forward(self, inputs, y_true):
+        # Output layer's activation function
+        self.activation.forward(inputs)
+        # Set the output
+        self.output = self.activation.output
+        # Calculate and return loss value
+        return self.loss.calculate(self.output, y_true)
+    
+    # Backward pass
+    def backward(self, dvalues, y_true):
+        # number of samples
+        samples = len(dvalues)
+
+        # If labels are one-hot encoded, turn them into discrete values
+        if len(y_true.shape) == 2:
+            y_true = np.argmax(y_true, axis=1)
+
+        # Copy so we can safely modify
+        self.dinputs = dvalues.copy()
+        # Calculate gradient
+        self.dinputs[range(samples), y_true] -= 1
+        # Normalize gradient
+        self.dinputs = self.dinputs / samples
+
+# softmax_loss = Activation_Softmax_Loss_CategoricalCrossentropy()
+# softmax_loss.backward(softmax_outputs1, class_targets1)
+# dvalues1 = softmax_loss.dinputs
+
+# activation = Activation_Softmax()
+# activation.output = softmax_outputs1
+# loss = Loss_CategoricalCrossentropy()
+# loss.backward(softmax_outputs1, class_targets1)
+# activation.backward(loss.dinputs)
+# dvalues2 = activation.dinputs
+
+# print('Gradients:  combined loss and activation:', dvalues1)
+# print('Gradients:  separate loss and activation:', dvalues2)
+
+# Create dataset
+X, y = spiral_data(samples=100, classes=3)
+
+# Create Dense layer with 2 input features and 3 output values
+dense1 = Layer_Dense(2, 3)
+
+# Create ReLU activation (to be used with Dense layer):
+activation1 = Activation_ReLU()
+
+# Create second Dense layer with 3 input features (as we take output
+# of previous layer here) and 3 output values (output values)
+dense2 = Layer_Dense(3, 3)
+
+# Create Softmax classifier's combined loss and activation
+loss_activation = Activation_Softmax_Loss_CategoricalCrossentropy()
+
+# Perform a forward pass of our training data through this layer
+dense1.forward(X)
+
+# Perform a forward pass through activation function
+# takes the output of first dense layer here
+activation1.forward(dense1.output)
+
+# Perform a forward pass through second Dense layer
+# takes outputs of activation function of first layer as inputs
+dense2.forward(activation1.output)
+
+# Perform a forward pass through the activation/loss function
+# takes the output of second dense layer here and returns loss
+loss = loss_activation.forward(dense2.output, y)
+
+# Let's see output of the first few samples:
+print(loss_activation.output[:5])
+
+# Print loss value
+print('loss:', loss)
+
+# Calculate accuracy from output of activation2 and targets
+# calculate values along first axis
+predictions = np.argmax(loss_activation.output, axis=1)
+if len(y.shape) == 2:
+    y = np.argmax(y, axis=1)
+accuracy = np.mean(predictions==y)
+
+# Print accuracy
+print('Accuracy:', accuracy)
+
+# Backward pass
+loss_activation.backward(loss_activation.output, y)
+dense2.backward(loss_activation.dinputs)
+activation1.backward(dense2.dinputs)
+dense1.backward(activation1.dinputs)
+
+# Print gradients
+print(dense1.dweights)
+print(dense1.dbiases)
+print(dense2.dweights)
+print(dense2.dbiases)
